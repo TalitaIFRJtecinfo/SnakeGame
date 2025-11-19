a534b8f401c328abd2fee8b3be0bcffe09f47b80
lavinia-ifrj <melolala09@gamil.com>
2025-11-19 11:26:41 -0300

    Criação do Snake Game

---
+++ placar.js
@@ -0,0 +1,21 @@
+const placar = {
+    largura: 800,
+    altura: 60,
+    corFundo:"grey",
+    corTexto:"black",
+    pontuacao:0,
+    nomeJogo: "IFRJ Snake",
+
+    desenhar(){
+        cCtx.fillStyle = this.corFundo;
+        cCtx.fillRect(0,0,this.largura,this.altura);
+        cCtx.fillStyle = this.corTexto;
+        cCtx.font = "20px Fantasy";
+        cCtx.textAlign = 'center';
+        cCtx.textBaseline = 'middle';
+        cCtx.fillText(this.nomeJogo,400,30);
+        cCtx.textAlign = 'left';
+        cCtx.textBaseline = 'bottom';
+        cCtx.fillText(this.pontuacao + " pts",10,50);
+    }
+}
